16ed03c95164687d73f7a2befd48000a8a230c6f
emreipsir13 <80037724+emreipsir13@users.noreply.github.com>
2021-05-11 00:05:32 +0300

    Add files via upload

---
+++ AimTraining/AimTraining/Form1.Designer.cs
@@ -0,0 +1,174 @@
+﻿
+namespace AimTraining
+{
+    partial class form13
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.Image = global::AimTraining.Properties.Resources._2_5;
+            this.button1.Location = new System.Drawing.Point(422, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(90, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Start / Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(108, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Correct Click";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(225, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Wrong Click";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(416, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(24, 16);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "00";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(448, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(24, 16);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "00";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.Location = new System.Drawing.Point(437, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 18);
+            this.label5.TabIndex = 7;
+            this.label5.Text = ":";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(331, 6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Reset";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // form13
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "form13";
+            this.Text = "Shot The Polat";
+            this.Click += new System.EventHandler(this.form13_Click);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ AimTraining/AimTraining/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AimTraining
+{
+    public partial class form13 : Form
+    {
+        int yansay = 0;
+        int dorsay = 0;
+        int sansay = 0;
+        int daksay = 0;
+        public form13()
+        {
+            InitializeComponent();
+            label1.Text = "Wrong Click\n" + yansay;
+            label2.Text = "Correct Click\n" + dorsay;
+        }
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            sansay++;
+            if (sansay == 60)
+            { daksay++; sansay = 0; label3.Text = Convert.ToString(daksay); }
+            label4.Text = Convert.ToString(sansay);
+        }
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            Random rnd = new Random();
+            int a = rnd.Next(12, 422);
+            int b = rnd.Next(44, 399);
+            button1.Location = new Point(a, b);
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled==false)
+            { timer1.Enabled = true;button1.Enabled = true; timer2.Enabled = true; }
+            else if (timer1.Enabled==true)
+            { timer1.Enabled = false;button1.Enabled = false; timer2.Enabled = false; }
+            Random rnd = new Random();
+            int a = rnd.Next(12, 422);
+            int b = rnd.Next(44, 399);
+            button1.Location = new Point(a, b);
+        }
+        private void form13_Click(object sender, EventArgs e)
+        {
+            yansay++;
+            label1.Text = "Wrong Click\n" + yansay;
+            Random rnd = new Random();
+            int a = rnd.Next(12, 422);
+            int b = rnd.Next(44, 399);
+            button1.Location = new Point(a, b);
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dorsay++;
+            label2.Text = "Correct Click\n" + dorsay;
+            Random rnd = new Random();
+            int a = rnd.Next(12, 422);
+            int b = rnd.Next(44, 399);
+            button1.Location = new Point(a, b);
+            timer2.Stop();
+            timer2.Start();
+        }
+        private void button3_Click(object sender, EventArgs e)
+        {
+            yansay = 0;
+            dorsay = 0;
+            daksay = 0;
+            sansay = 0;
+            label3.Text = "00";
+            label4.Text = "00";
+            label1.Text = "Wrong Click\n" + yansay;
+            label2.Text = "Correct Click\n" + dorsay;
+        }
+    }
+}
